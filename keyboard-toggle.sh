f61b3cba46e9c71effd1a7ffcbca83b99f9529cc
Reboot <reboot404.81@gmail.com>
2026-02-28 18:12:41 +0300

    ch

--- keyboard-toggle.sh
+++ keyboard-toggle.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# keyboard ID
-ID= 13 
+#Keyboard ID
+ID=13
 
-# icons directions
-ICON_DIR="$HOME/.local/share/icons/hicolor/256x256/apps"
+# icons directions 
+ICON_DIR="$HOME/.local/share/app-icons"
 ICON_OPEN="$ICON_DIR/keyboard.png"
-ICON_LOCK="$ICON_DIR/lock-keyboard.png"
+ICON_LOCK="$ICON_DIR/keyboard-lock.png"
 
 # desktop file
 DESKTOP_FILE="$HOME/Desktop/Keyboard Lock"
+
 
 STATE=$(xinput list-props "$ID" | grep "Device Enabled" | awk '{print $4}')
 
@@ -28,11 +29,11 @@ if [ -f "$DESKTOP_FILE" ]; then
     sed -i "s|^Icon=.*|Icon=$ICON_FILE|" "$DESKTOP_FILE"
 fi
 
-# refreshing the all KDE desktop icons
-:q
+# refresh the all KDE desktop icons
+
+if [DESKTOP_ENV="KDE"] 
 qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
 var allDesktops = desktops();
 for (i=0; i<allDesktops.length; i++) {
     allDesktops[i].reload();
 }"
-~                             
